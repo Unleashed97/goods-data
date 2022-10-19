fccb7c02a9a1cfc66362629011c93867a5e781eb
Nichkov Roman <nichkovroman@gmail.com>
2022-10-19 10:27:01 +0300

    feat: add remove btn, fix toggling cities

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// form
 const handleForm = () => {
     const formSubmitBtn = document.querySelector('.form__btn')
 
@@ -7,13 +8,15 @@ const handleForm = () => {
 }
 handleForm()
 
+// labels
 dates = ['26.01', '27.01', '28.01', '29.01', '30.01', '01.02', '02.02', '03.02', '04.02', '05.02', '06.02', '07.02', '08.02', '09.02', '10.02', '11.02', '12.02', '13.02', '14.02', '15.02', '16.02', '17.02', '18.02', '19.02', '20.02', '21.02', '22.02', '23.02', '24.02', '25.02']
 
+// for random data generation
 function createRandomNumber(min=1, max=10) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function fillDateGood(){
+function fillDataGood(){
     let data = []
     for(let i =0; i< dates.length; i++){
         data.push(createRandomNumber())
@@ -21,14 +24,17 @@ function fillDateGood(){
     return data
 }
 
-function Good(name){
-    this.name = name
-    this.dates = dates
-    this.data = fillDateGood()
+// color generation
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
-const obj1 = new Good('чайник заворочный')
-
+// chart
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'line',
@@ -36,49 +42,25 @@ const myChart = new Chart(ctx, {
         labels: dates,
         datasets: [{
             label: 'чайник заварочный',
-            data: fillDateGood(),
-            backgroundColor: [
-                'green'
-            ],
-            borderColor: [
-                'green'
-            ],
-            borderWidth: 2,
+            data: [null, 16, 17],
+            borderColor: getRandomColor(),
             tension: 0.5
         }, {
             label: 'чайник заварочный стеклянный',
-            data: fillDateGood(),
-            backgroundColor: [
-                'yellow',
-            ],
-            borderColor: [
-                '#ddd',
-            ],
-            borderWidth: 2,
+            data: fillDataGood(),
+            borderColor: getRandomColor(),
             tension: 0.5
         },
         {
             label: 'заварочный чайник',
-            data: fillDateGood(),
-            backgroundColor: [
-                'blue'
-            ],
-            borderColor: [
-                'blue'
-            ],
-            borderWidth: 2,
+            data: fillDataGood(),
+            borderColor: getRandomColor(),
             tension: 0.5
         },
         {
             label: 'заварочный чайник стекло',
-            data: fillDateGood(),
-            backgroundColor: [
-                'red',
-            ],
-            borderColor: [
-                'red',
-            ],
-            borderWidth: 2,
+            data: fillDataGood(),
+            borderColor: getRandomColor(),
             tension: 0.5,
         }]
     },
@@ -88,19 +70,21 @@ const myChart = new Chart(ctx, {
             yAxis: {
               position: 'right',
               reverse: true
-            }
+            },
+            
           },
         plugins: {
             legend: {
                 display: true,
                 position: 'bottom',
+                title: {
+                    padding: 20,
+                },
                 labels: {
+                    padding: 10,
 
                     usePointStyle: true,
-                    boxWidth: 3,
-
                     pointStyle: 'circle',
-
                 }
             }
         }
@@ -126,21 +110,7 @@ const handleTable = () => {
         }
     })
 }
-
 handleTable()
-
-// avg value for column
-
-// get column
-
-const avgValues = () => {
-    const avgRows = document.querySelectorAll('.avg')
-    
-
-    // avgRows.forEach()
-}
-
-avgValues()
 
 // toggle cities
 const toggleCities = () => {
@@ -152,16 +122,42 @@ const toggleCities = () => {
 
             function nextSibling(parent){
                 let nextEl = parent.nextElementSibling
-                nextEl.classList.toggle('hide')
 
-                return nextEl
+                if(nextEl && nextEl.classList.contains('good-city')){
+                    nextEl.classList.toggle('hide')
+                    nextSibling(nextEl)
+
+                } 
+                else return 
             }
 
-            for(let i =0; i< 7; i++){
-                parent = nextSibling(parent)
-            }
+            nextSibling(parent)
         })
     })
 
 }
 toggleCities()
+
+
+const handleRemove = () => {
+    const goodRemoveBtnList = document.querySelectorAll('.good-remove')
+
+    goodRemoveBtnList.forEach(btn => btn.addEventListener('click', (e) => {
+        e.stopPropagation()
+
+        const serObj ={}
+        const goodRow = btn.closest('.good-row')
+
+        const art = goodRow.querySelector('.good-art')
+        const name = goodRow.querySelector('.table__cell-name')
+
+        serObj.art = art.innerText
+        serObj.name = name.innerText
+
+        console.log(serObj)
+
+    }))
+
+}
+
+handleRemove()
